0f73878adbe5b9f8e33e75ace6bbedeccdbe1930
Pabline Ribeiro <119081902+pablinesamara@users.noreply.github.com>
2024-05-10 14:15:39 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan;
+    public static void main(String[] args) {
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o seu nome :");
+        String nome = sc.nextLine();
+        System.out.println("Informe a sua idade :");
+        int idade = sc.nextInt();
+        ArrayList<String>nomes= new ArrayList<String>();*/
+        scan=new Scanner(System.in);
+        ArrayList<Integer>idades=new ArrayList<Integer>();
+        for (int i=0;i<=idades.size();i++){
+            System.out.println("Digite o valor da posição : "+i);
+            idades.add(scan.nextInt());
+            System.out.println("ESTE É O MEU ARRAYLIST: "+idades);
+        }
+
+
+    }
+}
